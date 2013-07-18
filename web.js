4bdf502947b30e3de3e2c7c1c8a7dc14709f6fea
sbua2003@yahoo.com <sbua2003@yahoo.com>
2013-07-18 18:42:17 +0000

    web.js to use fs and buffer

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
  var buf = new Buffer(fs.readFileSync('./index.html'));
-  response.send(buf);
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
